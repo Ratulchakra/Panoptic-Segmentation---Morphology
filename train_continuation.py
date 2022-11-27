4a20ab55031139794b218b23ccc6a32d271a7e63
Ratul Chakraborty <105482346+Ratulchakra@users.noreply.github.com>
2022-11-27 12:37:20 +0530

    Add files via upload

--- train_continuation.py
+++ train_continuation.py
@@ -9,11 +9,14 @@ import torch.nn as  nn
 torch.cuda.empty_cache()
 
 from data_loader.coco import COCOSegmentation
-from tweaked_unet import UNet, mse_ce_combo
 
+from losses import * 
+from unet_model  import * 
 import torch.optim as optim
 from torch.utils.data import DataLoader
 import argparse
+
+from tqdm import tqdm
 
 #######################
 
@@ -36,7 +39,7 @@ coco_val=COCOSegmentation(args, split='val', year='2017')
 val_loader = DataLoader(coco_val, batch_size=batch_size, shuffle=True, num_workers=0) 
 
 ##########################
-model = torch.load("/home/sysadm/Documents/ratul_env/Morphology/unet_trained_50_COCO.pt")
+model = torch.load("/home/sysadm/Documents/ratul_env/Morphology/remodeled_unet_41_COCO.pt")
 ##########################
 
 
@@ -50,8 +53,9 @@ with torch.no_grad():
     for j in range(epochs):
         train_loss, val_loss = 0, 0
         model.train()
-        for i, sample in enumerate(train_loader):
-
+        n = len(train_loader)
+        tbar = tqdm(enumerate(train_loader), total=n)
+        for i, sample in tbar:
             I=sample['image']
             seg_gt = sample['label_seg']
             # inst_gt = sample['label_inst']
@@ -62,34 +66,22 @@ with torch.no_grad():
 
             optimizer.zero_grad()
             pred_out=model(I)
-            loss = mse_ce_combo(pred_out, seg_gt, alpha=0.5)
+            loss=torch.nn.CrossEntropyLoss()(pred_out,seg_gt.long()) + focal_tv_loss(pred_out, seg_gt)
             loss.requires_grad_()
             loss.backward()
             optimizer.step()
             train_loss += loss.item()
-        model.eval()
-        for i, sample in enumerate(val_loader):
-            I=sample['image']
-            seg_gt = sample['label_seg']
+            tbar.set_description('Training Now Batch: {}/{} , with Batch Loss {}'. \
+                        format(i, n, loss.item()) )
 
-            I=I.to(device)
-            seg_gt=seg_gt.to(device)
-
-            pred_out=model(I)
-            loss = mse_ce_combo(pred_out, seg_gt, alpha=0.5)
-            val_loss += loss.item()
-
-        train_loss_rec.append(train_loss)
-        val_loss_rec.append(val_loss)
-
-        print(f"For Epoch {j+1} train loss {train_loss} and validation loss {val_loss}")
-        if (j+1) % 10 == 0:
-        	torch.save(model, f"/home/sysadm/Documents/ratul_env/Morphology/unet_trained_{j+1}_COCO.pt")
-        	print(f"Model saved after {j+1} epochs........")
+        train_loss_rec.append(train_loss/n)
+        print(f"For Epoch {j+1} train loss {train_loss_rec[-1]}")
+        if (j+1) % 30 == 0:
+        	torch.save(model, f"/home/sysadm/Documents/ratul_env/Morphology/remodeled_unet_trained_{j+1}_COCO.pt")
 
 print("Training Ends-----")
 
-torch.save(model, f"/home/sysadm/Documents/ratul_env/Morphology/unet_trained_{epochs}_COCO.pt")
+torch.save(model, f"/home/sysadm/Documents/ratul_env/Morphology/remodeled_unet_trained_{epochs}_COCO.pt")
 print("Model Saved")
 
 plt.plot(range(len(train_loss_rec)), train_loss_rec)
